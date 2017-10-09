7b4334f51b86c3fc124cbabb5464fa1f345e1033
Luka Bratos <luka@pusher.com>
2017-10-09 14:26:22 +0100

    Cleanup

--- errol-ios/errol-ios/ViewController.swift
+++ errol-ios/errol-ios/ViewController.swift
@@ -1,25 +1,12 @@
-//
-//  ViewController.swift
-//  errol-ios
-//
-//  Created by Luka on 09/10/2017.
-//  Copyright © 2017 Pusher. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
-
