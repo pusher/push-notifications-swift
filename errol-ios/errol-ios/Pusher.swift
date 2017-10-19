1d0852e3ab2e5652fb6728e76981d48f49c1f2a4
Luka Bratos <luka@pusher.com>
2017-10-19 12:43:27 +0100

    Move registration logic to the Pusher struct

--- errol-ios/errol-ios/Pusher.swift
+++ errol-ios/errol-ios/Pusher.swift
@@ -2,4 +2,12 @@ import Foundation
 
 struct Pusher {
     let instanceId: String
+
+    public func register(deviceToken: Data) {
+        let session = URLSession.shared
+        let url = URL.init(string: "https://errol-staging.herokuapp.com/device_api/v1/instances/\(instanceId)/devices/apns")!
+        let networkService = NetworkService.init(url: url, session: session)
+
+        networkService.register(deviceToken: deviceToken)
+    }
 }
--- errol-ios/errol-ios/ViewController.swift
+++ errol-ios/errol-ios/ViewController.swift
@@ -5,12 +5,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let session = URLSession.shared
-        let url = URL.init(string: "https://errol-staging.herokuapp.com/device_api/v1/instances/c0c65938-5dcf-4e8b-9206-f72a8d86684b/devices/apns")!
-        let networkService = NetworkService.init(url: url, session: session)
-
-        networkService.register(deviceToken: "dddd")
     }
 
     override func didReceiveMemoryWarning() {
