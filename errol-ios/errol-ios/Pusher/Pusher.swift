3afc00d807c5c80018cf6a4bb352e8a9c2515019
Luka Bratos <luka@pusher.com>
2017-10-19 13:11:59 +0100

    Remove init

--- errol-ios/errol-ios/Pusher/Pusher.swift
+++ errol-ios/errol-ios/Pusher/Pusher.swift
@@ -10,8 +10,8 @@ struct Pusher {
 
     public func registerDeviceToken(_ deviceToken: Data) {
         let session = URLSession.shared
-        let url = URL.init(string: "https://errol-staging.herokuapp.com/device_api/v1/instances/\(instanceId)/devices/apns")!
-        let networkService = NetworkService.init(url: url, session: session)
+        let url = URL(string: "https://errol-staging.herokuapp.com/device_api/v1/instances/\(instanceId)/devices/apns")!
+        let networkService = NetworkService(url: url, session: session)
 
         networkService.register(deviceToken: deviceToken)
     }
