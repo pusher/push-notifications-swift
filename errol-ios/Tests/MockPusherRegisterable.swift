b0ebbe5243bf1c2b8e8254d64b837bfcce4a7301
Luka Bratos <luka@pusher.com>
2017-10-20 16:29:24 +0100

    Tidy up

--- errol-ios/Tests/MockPusherRegisterable.swift
+++ errol-ios/Tests/MockPusherRegisterable.swift
@@ -2,7 +2,7 @@ import Foundation
 @testable import errol_ios
 
 struct MockPusherRegisterable: PusherRegisterable {
-    func register(deviceToken: Data, completion: @escaping PusherRegisterable.CompletionHandler) {
+    func register(deviceToken: Data, completion: @escaping CompletionHandler) {
         completion("ppns-876eeb5d-0dc8-4d74-9f59-b65412b2c742")
     }
 }
