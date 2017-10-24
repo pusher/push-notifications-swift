b750136a67f05b6e3276f2c105cb41414c82d87f
Luka Bratos <luka@pusher.com>
2017-10-24 09:35:02 +0100

    Add script to generate Jazzy docs

---
+++ generate_docs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+jazzy \
+--source-directory Errol \
+--author Pusher \
+--author_url https://pusher.com \
+--github_url https://github.com/pusher/errol-ios \
+--output docs/swift_output \
