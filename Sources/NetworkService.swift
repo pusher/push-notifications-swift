3db5d6538ceed962d9625974545e30a1ba3ca9d3
Luka Bratos <luka@pusher.com>
2017-10-26 17:31:52 +0100

    Remove platformType

--- Sources/NetworkService.swift
+++ Sources/NetworkService.swift
@@ -11,7 +11,7 @@ struct NetworkService: ErrolRegisterable, ErrolSubscribable {
     func register(deviceToken: Data, completion: @escaping CompletionHandler) {
         let deviceTokenString = deviceToken.hexadecimalRepresentation()
         let bundleIdentifier = Bundle.main.bundleIdentifier ?? ""
-        let bodyString = "{\"platformType\": \"apns\", \"token\": \"\(deviceTokenString)\", \"bundleIdentifier\": \"\(bundleIdentifier)\"}"
+        let bodyString = "{\"token\": \"\(deviceTokenString)\", \"bundleIdentifier\": \"\(bundleIdentifier)\"}"
         guard let body = bodyString.data(using: .utf8) else { return }
         let request = self.setRequest(url: self.url, httpMethod: .POST, body: body)
 
