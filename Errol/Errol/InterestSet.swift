0a110247d6cb611ff8f9d82dfb35cca46a603469
Luka Bratos <luka@pusher.com>
2017-11-03 09:54:53 +0000

    Remove CodingKeys

--- Errol/Errol/InterestSet.swift
+++ Errol/Errol/InterestSet.swift
@@ -2,10 +2,5 @@ import Foundation
 
 struct InterestSet: Decodable {
     var interests: Array<String>
-    var metadata: Dictionary<String, String>
-
-    private enum CodingKeys : String, CodingKey {
-        case interests = "interests"
-        case metadata = "responseMetadata"
-    }
+    var responseMetadata: Dictionary<String, String>
 }
