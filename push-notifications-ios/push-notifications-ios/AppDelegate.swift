488d5a4d23429cadf5efc497688043f69d84b5fa
Luka Bratos <luka.bratos@gmail.com>
2018-01-24 10:28:52 +0000

    Create push-notifications-ios project

--- push-notifications-ios/push-notifications-ios/AppDelegate.swift
+++ push-notifications-ios/push-notifications-ios/AppDelegate.swift
@@ -3,16 +3,16 @@ import PushNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     let pushNotifications = PushNotifications.shared
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         self.pushNotifications.register(instanceId: "e3c54800-e058-4e75-9e20-424f7dddce30")
-
+        
         return true
     }
-
+    
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         self.pushNotifications.registerDeviceToken(deviceToken) {
             self.pushNotifications.subscribe(interest: "hello")
--- push-notifications-ios/push-notifications-ios/ViewController.swift
+++ push-notifications-ios/push-notifications-ios/ViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 import Foundation
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
