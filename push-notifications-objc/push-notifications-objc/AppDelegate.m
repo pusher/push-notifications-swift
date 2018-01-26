88dfa74294253a053baf324a119bf9ddd97f8923
Luka Bratos <luka@pusher.com>
2018-01-26 15:49:21 +0000

    Add iOS with Objective-C example

---
+++ push-notifications-objc/push-notifications-objc/AppDelegate.m
@@ -0,0 +1,38 @@
+#import "AppDelegate.h"
+@import PushNotifications;
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [[PushNotifications shared] registerWithInstanceId:@"97c56dfe-58f5-408b-ab3a-158e51a860f2" application:application];
+
+    return YES;
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    [[PushNotifications shared] registerDeviceToken:deviceToken completion:^{
+        NSError *anyError;
+        [[PushNotifications shared] subscribeWithInterest:@"hello" error:&anyError completion:^{
+            if (anyError) {
+                NSLog(@"Error: %@", anyError);
+            }
+            else {
+                NSLog(@"Subscribed to interest hello.");
+            }
+        }];
+    }];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    NSLog(@"%@", userInfo);
+}
+
+-(void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    NSLog(@"Remote notification support is unavailable due to error: %@", error.localizedDescription);
+}
+
+@end
