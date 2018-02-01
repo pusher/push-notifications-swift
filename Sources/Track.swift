13b2d8c3e1e2e8f8b0ad083967ac347a8693cee1
Luka Bratos <luka@pusher.com>
2018-02-01 15:43:10 +0000

    Add device id and event type

--- Sources/Track.swift
+++ Sources/Track.swift
@@ -3,4 +3,6 @@ import Foundation
 struct Track: Encodable {
     let publishId: String
     let timestampMs: Int64
+    let eventType: String
+    let deviceId: String
 }
--- Tests/TrackTests.swift
+++ Tests/TrackTests.swift
@@ -2,19 +2,21 @@ import XCTest
 @testable import PushNotifications
 
 class TrackTests: XCTestCase {
-    let track: Track = Track(publishId: "123", timestampMs: 123456789)
+    let track: Track = Track(publishId: "123", timestampMs: 123456789, eventType: "Delivery", deviceId: "123")
 
     func testTrackModel() {
         let track = self.track
         XCTAssertNotNil(track)
         XCTAssertEqual(track.publishId, "123")
         XCTAssertEqual(track.timestampMs, 123456789)
+        XCTAssertEqual(track.eventType, "Delivery")
+        XCTAssertEqual(track.deviceId, "123")
     }
 
     func testTrackEncoded() {
         let trackEncoded = try! self.track.encode()
         XCTAssertNotNil(trackEncoded)
-        let trackJSONExpected = "{\"timestampMs\":123456789,\"publishId\":\"123\"}"
+        let trackJSONExpected = "{\"deviceId\":\"123\",\"eventType\":\"Delivery\",\"publishId\":\"123\",\"timestampMs\":123456789}"
         let trackJSON = String(data: trackEncoded, encoding: .utf8)!
         XCTAssertEqual(trackJSONExpected, trackJSON)
     }
