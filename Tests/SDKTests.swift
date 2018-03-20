e3678df01a9d0c416ac119147484c19cfab8eada
Luka Bratos <luka@pusher.com>
2018-03-20 10:46:09 +0000

    Bump version to 0.10.5

--- Tests/SDKTests.swift
+++ Tests/SDKTests.swift
@@ -5,6 +5,6 @@ class SDKTests: XCTestCase {
     func testVersionModel() {
         let version = SDK.version
         XCTAssertNotNil(version)
-        XCTAssertEqual(version, "0.10.4")
+        XCTAssertEqual(version, "0.10.5")
     }
 }
