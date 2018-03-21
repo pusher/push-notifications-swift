b755f0794de08f36df6878027fe6a2a881873788
Luka Bratos <luka@pusher.com>
2018-03-21 09:44:19 +0000

    Update tests

--- Tests/PushNotificationsNetworkableTests.swift
+++ Tests/PushNotificationsNetworkableTests.swift
@@ -9,23 +9,23 @@ class PushNotificationsNetworkableTests: XCTestCase {
 
     var networkService: PushNotificationsNetworkable!
 
-//    func testRegistration() {
-//        let url = URL(string: "https://\(instanceId).pushnotifications.pusher.com/device_api/v1/instances/\(instanceId)/devices/apns")!
-//        let networkService = NetworkService(url: url, session: URLSession.shared)
-//        let exp = expectation(description: "It should successfully register the device")
-//        let deviceTokenData = "e4cea6a8b2419499c8c716bec80b705d7a5d8864adb2c69400bab9b7abe43ff1".toData()!
-//        networkService.register(deviceToken: deviceTokenData, instanceId: instanceId) { (deviceId) in
-//            XCTAssert(deviceId == "apns-8792dc3f-45ce-4fd9-ab6d-3bf731f813c6")
-//            XCTAssert(true)
-//            exp.fulfill()
-//        }
-//
-//        waitForExpectations(timeout: 1) { error in
-//            if let error = error {
-//                XCTFail("waitForExpectations errored: \(error)")
-//            }
-//        }
-//    }
+    func testRegistration() {
+        let url = URL(string: "https://\(instanceId).pushnotifications.pusher.com/device_api/v1/instances/\(instanceId)/devices/apns")!
+        let networkService = NetworkService(url: url, session: URLSession.shared)
+        let exp = expectation(description: "It should successfully register the device")
+        let deviceTokenData = "e4cea6a8b2419499c8c716bec80b705d7a5d8864adb2c69400bab9b7abe43ff1".toData()!
+        networkService.register(deviceToken: deviceTokenData, instanceId: instanceId) { (deviceId) in
+            XCTAssert(deviceId == "apns-8792dc3f-45ce-4fd9-ab6d-3bf731f813c6")
+            XCTAssert(true)
+            exp.fulfill()
+        }
+
+        waitForExpectations(timeout: 1) { error in
+            if let error = error {
+                XCTFail("waitForExpectations errored: \(error)")
+            }
+        }
+    }
 
     func testSubscribe() {
         let url = URL(string: "https://\(instanceId).pushnotifications.pusher.com/device_api/v1/instances/\(instanceId)/devices/apns/\(deviceId)/interests/\(interest)")!
@@ -86,15 +86,33 @@ class PushNotificationsNetworkableTests: XCTestCase {
     func testTrack() {
         let url = URL(string: "https://\(instanceId).pushnotifications.pusher.com/reporting_api/v1/instances/\(instanceId)/events")!
         let networkService = NetworkService(url: url, session: URLSession.shared)
-        let userInfo = ["aps": ["alert": "hello"]]
-        networkService.track(userInfo: userInfo, eventType: ReportEventType.Delivery.rawValue, deviceId: "abc")
-        XCTAssert(true) // Add integration test.
+        let userInfo = ["data": ["pusher": ["publishId": "1"]]]
+        let exp = expectation(description: "It should successfully track notification")
+        networkService.track(userInfo: userInfo, eventType: ReportEventType.Delivery.rawValue, deviceId: "abc") {
+            XCTAssert(true)
+            exp.fulfill()
+        }
+
+        waitForExpectations(timeout: 1) { error in
+            if let error = error {
+                XCTFail("waitForExpectations errored: \(error)")
+            }
+        }
     }
 
     func testMetadata() {
         let url = URL(string: "https://\(instanceId).pushnotifications.pusher.com/device_api/v1/instances/\(instanceId)/devices/apns/\(deviceId)/metadata")!
         let networkService = NetworkService(url: url, session: URLSession.shared)
-        networkService.syncMetadata()
-        XCTAssert(true) // Add integration test.
+        let exp = expectation(description: "It should successfully sync outdated metadata")
+        networkService.syncMetadata {
+            XCTAssert(true)
+            exp.fulfill()
+        }
+
+        waitForExpectations(timeout: 1) { error in
+            if let error = error {
+                XCTFail("waitForExpectations errored: \(error)")
+            }
+        }
     }
 }
