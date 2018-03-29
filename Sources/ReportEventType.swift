675605ddec375e19bc36b0939906bf31d2237d6b
Luka Bratos <luka@pusher.com>
2018-03-29 16:20:53 +0100

    Simplify ReportEventType

--- Sources/ReportEventType.swift
+++ Sources/ReportEventType.swift
@@ -1,30 +1,37 @@
 import Foundation
 
-class ReportEventType: Encodable {
-    let eventId: String
+protocol ReportEventType: Encodable {}
+
+struct OpenEventType: ReportEventType {
+    let event: String
     let publishId: String
     let deviceId: String
     let timestampSecs: UInt
 
     init(eventId: String = "Open", publishId: String, deviceId: String, timestampSecs: UInt) {
-        self.eventId = eventId
+        self.event = eventId
         self.publishId = publishId
         self.deviceId = deviceId
         self.timestampSecs = timestampSecs
     }
 }
 
-typealias OpenEventType = ReportEventType
-
-class DeliveryEventType: ReportEventType {
+struct DeliveryEventType: ReportEventType {
+    let event: String
+    let publishId: String
+    let deviceId: String
+    let timestampSecs: UInt
     let appInBackground: Bool
     let hasDisplayableContent: Bool
     let hasData: Bool
 
     init(eventId: String = "Delivery", publishId: String, deviceId: String, timestampSecs: UInt, appInBackground: Bool, hasDisplayableContent: Bool, hasData: Bool) {
+        self.event = eventId
+        self.publishId = publishId
+        self.deviceId = deviceId
+        self.timestampSecs = timestampSecs
         self.appInBackground = appInBackground
         self.hasDisplayableContent = hasDisplayableContent
         self.hasData = hasData
-        super.init(eventId: eventId, publishId: publishId, deviceId: deviceId, timestampSecs: timestampSecs)
     }
 }
