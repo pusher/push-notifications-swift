a5404ba5f0aa178be2d4062488215897e20d2678
Luka Bratos <luka@pusher.com>
2018-05-31 10:32:45 +0100

    Add initial interest set to the device

--- Sources/Device.swift
+++ Sources/Device.swift
@@ -2,6 +2,7 @@ import Foundation
 
 struct Device: Decodable {
     var id: String
+    var initialInterestSet: Array<String>?
 
     static func persist(_ deviceId: String) {
         UserDefaults(suiteName: "PushNotifications")?.set(deviceId, forKey: "com.pusher.sdk.deviceId")
