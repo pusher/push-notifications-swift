0beec47dcbbab103dc6e7a7dfd3a6b9e0e610d0a
Luka Bratos <luka@pusher.com>
2018-07-17 11:47:11 +0100

    Do not use CommonCrypto for calculating MD5

--- Tests/InterestsMD5HashTests.swift
+++ Tests/InterestsMD5HashTests.swift
@@ -4,9 +4,9 @@ import XCTest
 class InterestsMD5HashTests: XCTestCase {
     func testCalculatedHashIsCorrect() {
         let interestsMD5Hash1 = ["a", "b", "c", "d", "e"].calculateMD5Hash()
-        XCTAssertEqual(interestsMD5Hash1, "fb2ae5db06efd8297195270bdc4fb60b")
+        XCTAssertEqual(interestsMD5Hash1!, "FB2AE5DB06EFD8297195270BDC4FB60B")
 
         let interestsMD5Hash2 = ["vegan-pizza", "donuts", "zzz", "aaa", "a"].calculateMD5Hash()
-        XCTAssertEqual(interestsMD5Hash2, "749e2830366ba863f796cdf5e281662f")
+        XCTAssertEqual(interestsMD5Hash2!, "749E2830366BA863F796CDF5E281662F")
     }
 }
