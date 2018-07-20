8c252c5db52a6a0bfdda0a707e3df21a6dbea2ab
Luka Bratos <luka@pusher.com>
2018-07-20 09:50:55 +0100

    Update tests

--- Tests/InterestsMD5HashTests.swift
+++ Tests/InterestsMD5HashTests.swift
@@ -4,9 +4,9 @@ import XCTest
 class InterestsMD5HashTests: XCTestCase {
     func testCalculatedHashIsCorrect() {
         let interestsMD5Hash1 = ["a", "b", "c", "d", "e"].calculateMD5Hash()
-        XCTAssertEqual(interestsMD5Hash1!, "FB2AE5DB06EFD8297195270BDC4FB60B")
+        XCTAssertEqual(interestsMD5Hash1, "FB2AE5DB06EFD8297195270BDC4FB60B")
 
         let interestsMD5Hash2 = ["vegan-pizza", "donuts", "zzz", "aaa", "a"].calculateMD5Hash()
-        XCTAssertEqual(interestsMD5Hash2!, "749E2830366BA863F796CDF5E281662F")
+        XCTAssertEqual(interestsMD5Hash2, "749E2830366BA863F796CDF5E281662F")
     }
 }
