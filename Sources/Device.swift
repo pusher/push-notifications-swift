8aa7b297ab06fccfb1805c889f3496d971ee480d
Luka Bratos <luka@pusher.com>
2018-09-11 11:02:30 +0100

    Add logic for the device token persistence

--- Sources/Device.swift
+++ Sources/Device.swift
@@ -12,6 +12,18 @@ struct Device: Decodable {
         return UserDefaults(suiteName: Constants.UserDefaults.suiteName)?.string(forKey: Constants.UserDefaults.deviceId)
     }
 
+    static func persist(deviceToken: String) {
+        UserDefaults(suiteName: Constants.UserDefaults.suiteName)?.set(deviceToken, forKey: Constants.UserDefaults.deviceToken)
+    }
+
+    static func tokenHasChanged(deviceToken: String) -> Bool {
+        guard let persistedDeviceToken = UserDefaults(suiteName: Constants.UserDefaults.suiteName)?.string(forKey: Constants.UserDefaults.deviceToken) else {
+            return true
+        }
+
+        return deviceToken != persistedDeviceToken
+    }
+
     static func idAlreadyPresent() -> Bool {
         return self.getDeviceId() != nil
     }
