e387e0defd4ab2c0f83d89b4dbf8c4816bb74b3b
Danielle Vass <danielle.vass@pusher.com>
2019-11-21 10:22:16 +0000

    Remove prints from the InstanceId

--- PushNotifications/PushNotifications/InstanceId.swift
+++ PushNotifications/PushNotifications/InstanceId.swift
@@ -4,14 +4,8 @@ struct InstanceId {
     let id: String?
 
     init(userInfo: [AnyHashable: Any]) {
-        
-        print(userInfo)
-        
         let data = userInfo["data"] as? [String: Any]
         let pusher = data?["pusher"] as? [String: Any]
         self.id = pusher?["instanceId"] as? String
-        
-        print(self.id)
-        
     }
 }
