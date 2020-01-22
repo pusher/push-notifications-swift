1cbbc6b6105440cb7bad45a801f2ac71d8bfa43d
Danielle Vass <danielle.vass@pusher.com>
2020-01-22 17:11:04 +0000

    Demonstrate using multiple instance ids using the demo objc app

--- push-notifications-objc/push-notifications-objc/AppDelegate.m
+++ push-notifications-objc/push-notifications-objc/AppDelegate.m
@@ -8,11 +8,24 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    // if using one instance id throughout the whole app do the following:
     [[PushNotifications shared] startWithInstanceId:@"YOUR_INSTANCE_ID"]; // Can be found here: https://dash.pusher.com
     [[PushNotifications shared] registerForRemoteNotifications];
 
     NSError *anyError;
     [[PushNotifications shared] addDeviceInterestWithInterest:@"debug-test" error:&anyError];
+    
+    // if using multiple instances do the following:
+//    PushNotifications *pn1 = [[PushNotifications alloc] initWithInstanceId: @"YOUR_INSTANCE_ID_1"];
+//    NSError *anyError;
+//    [pn1 addDeviceInterestWithInterest:@"debug-potatoes" error:&anyError]
+//
+//    PushNotifications *pn2 = [[PushNotifications alloc] initWithInstanceId: @"YOUR_INSTANCE_ID_2"];
+//    NSError *anyError;
+//    [pn2 addDeviceInterestWithInterest:@"debug-carrots" error:&anyError]
+    
+    
 
     return YES;
 }
