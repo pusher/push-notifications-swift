f5dec71324fd90b059761ffacb7ff5f0ce028a35
Daniel Browne <danbrowne@pusher.com>
2021-01-06 10:21:09 +0000

    Update dependencies to their latest versions

--- Package.swift
+++ Package.swift
@@ -11,9 +11,9 @@ let package = Package(name: "PushNotifications",
                       ],
                       dependencies: [
                         .package(url: "https://github.com/Quick/Nimble",
-                                 .upToNextMajor(from: "8.1.2")),
-                        .package(url: "https://github.com/AliSoftware/OHHTTPStubs",
                                  .upToNextMajor(from: "9.0.0")),
+                        .package(url: "https://github.com/AliSoftware/OHHTTPStubs",
+                                 .upToNextMajor(from: "9.1.0")),
                       ],
                       targets: [
                         .target(name: "PushNotifications",
