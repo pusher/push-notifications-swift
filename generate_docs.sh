962b8898eb15c425830701e1446873603691469f
Daniel Browne <daniel.browne@pusher.com>
2021-07-05 15:47:56 +0100

    Improve autogenerated API docs script

--- generate_docs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-jazzy \
---clean \
---source-directory PushNotifications \
---author Pusher \
---author_url https://pusher.com \
---github_url https://github.com/pusher/push-notifications-swift \
---output docs/ \
