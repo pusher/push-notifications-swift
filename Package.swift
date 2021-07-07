ed8f66dc9f232fe3827524a95937124d04f8a4dc
Daniel Browne <daniel.browne@pusher.com>
2021-07-07 12:49:40 +0100

    Add SwiftLint as a non-target SPM dependency
    - Useful for local linting during development when working on the Package directly (rather than via the Xcode workspace)

--- Package.swift
+++ Package.swift
@@ -14,6 +14,9 @@ let package = Package(name: "PushNotifications",
                                  .upToNextMajor(from: "9.0.0")),
                         .package(url: "https://github.com/AliSoftware/OHHTTPStubs",
                                  .upToNextMajor(from: "9.1.0")),
+                        // Source code linting
+                        .package(url: "https://github.com/realm/SwiftLint",
+                                 .upToNextMajor(from: "0.43.1"))
                       ],
                       targets: [
                         .target(name: "PushNotifications",
